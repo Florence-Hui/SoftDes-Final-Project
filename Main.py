a88da50c30bbc9b9188f56857c5127290ddae46e
haikim2 <khaihihi06@gmail.com>
2026-04-18 14:28:38 -0400

    Checked intial user interface

--- Main.py
+++ Main.py
@@ -7,11 +7,20 @@ from Controller import Controller
 pygame.init()
 game = GameModel()
 view = View(game)
+controller = Controller(game)
+
+screen = pygame.display.set_mode((400, 500))
+pygame.display.set_caption("2048")
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        
-        
+    view.screen = screen
+    view.draw_grid()
+    view.show_score()
+
+    pygame.display.flip()
+
+pygame.quit()
