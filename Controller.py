1780f3507aedb9dd7887d8708f5b6c6386feff9a
Ben <bzmeyer18@gmail.com>
2026-04-18 12:57:26 -0400

    added docstrings to Controller.py

--- Controller.py
+++ Controller.py
@@ -1,3 +1,7 @@
+'''
+This contains the class for the controller of a custom 2048 game.
+'''
+
 from pygame.locals import (
     K_UP, K_DOWN, K_LEFT, K_RIGHT,
     K_w, K_s, K_a, K_d,
@@ -6,21 +10,52 @@ from pygame.locals import (
 
 
 class Controller():
+    '''
+    A class that reads user input and calls corresponding methods in the
+    game class for a custom 2048 game.
+    
+    Attributes:
+        model: An instance of the GameModel class containing information
+        about the current 2048 game being played.
+        
+        keys_pressed: A dictionary in which the keys represent keys on a
+        keyboard, which are mapped to boolean values that are True if the
+        key is being pressed and False otherwise.
+    '''
     
     def __init__(self, model):
+        '''
+        Initializes the Controller class.
+        
+        Args:
+            model: An instance of the GameModel class containing information
+            about the current 2048 game being played.
+        '''
         self.model = model
         self.keys_pressed = {}
     
     def check_inputs(self, keys_pressed):
+        '''
+        Checks all inputs for the game, and calls corresponding methods
+        when a key is pressed.
+        '''
         if not self.game.check_transition(): #CHANGE BASED ON GAME METHOD
             self.keys_pressed = keys_pressed
             
-            is_moving = self.check_movement()
+            is_moving = self._check_movement()
             
             if not is_moving:
-                self.check_other_inputs()
+                self._check_other_inputs()
     
-    def check_movement(self):
+    def _check_movement(self):
+        '''
+        Checks for presses of the movement keys, both WASD and the arrow keys.
+        Calls the method for moving tiles if a key is being pressed.
+        
+        Returns:
+            A boolean that is true if an movement key is being pressed,
+            or false otherwise.
+        '''
         key = ""
         
         if self.keys_pressed[K_UP] or self.keys_pressed[K_w]:
@@ -36,7 +71,10 @@ class Controller():
         return key != ""
         # CHANGE move_tiles TO WHATEVER THE CORRESPONDING METHOD IN THE GAME CLASS IS
     
-    def check_other_inputs(self):
+    def _check_other_inputs(self):
+        '''
+        Checks the space bar and escape key for inputs from the user
+        '''
         if self.keys_pressed[K_SPACE]:
             pass    # fill in menu selection functionality later
         elif self.keys_pressed[K_ESCAPE]:
